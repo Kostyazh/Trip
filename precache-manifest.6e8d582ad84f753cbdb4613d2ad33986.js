ec65f9122c567e560f4f39abdeb04bbc9aaa159d
Kostyazh <zhebelev.konstantin@yandex.ua>
2020-09-22 13:50:10 +0300

    dist

---
+++ precache-manifest.6e8d582ad84f753cbdb4613d2ad33986.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a246c4b72327a84a018",
+    "url": "/Trip/css/app.0445f927.css"
+  },
+  {
+    "revision": "0e21dbe8f4ea3151f651",
+    "url": "/Trip/css/chunk-0d8e03b4.1fb8613b.css"
+  },
+  {
+    "revision": "8788a86368423e18da7259cbd5c442d8",
+    "url": "/Trip/index.html"
+  },
+  {
+    "revision": "8a246c4b72327a84a018",
+    "url": "/Trip/js/app.ebd51eb7.js"
+  },
+  {
+    "revision": "0e21dbe8f4ea3151f651",
+    "url": "/Trip/js/chunk-0d8e03b4.c60a1966.js"
+  },
+  {
+    "revision": "73cee30f3e593fdd50c5",
+    "url": "/Trip/js/chunk-vendors.73ecb3ff.js"
+  },
+  {
+    "revision": "e8939ab30bf7e33d2948d3ea6c768f41",
+    "url": "/Trip/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Trip/robots.txt"
+  }
+]);
